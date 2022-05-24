cff72d72d4702b3d6b9decc499af7cbcbd71a178
Matheus-Venancio <matheusvecordeiro@gmail.com>
2022-05-23 23:06:56 -0300

    Initial commit

---
+++ app/src/main/java/com/example/mytag/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.mytag
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.mytag.ui.theme.MyTagTheme
+import com.google.firebase.database.ktx.database
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyTagTheme {
+                val db = Firebase.firestore
+                val database = Firebase.database
+                val myRef = database.getReference("message")
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MyTagTheme {
+        Greeting("Android")
+    }
+}
